c4ed661e01f3d0afe988f545d340685ef936b842
urbankreatives <117651600+urbankreatives@users.noreply.github.com>
2023-03-13 22:48:17 +0200

    kick3

--- server copy.js
+++ server copy.js
@@ -1,8 +1,6 @@
-//require('./seed/product-seeder');
 
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -14,17 +12,15 @@ var session = require('express-session');
 var validator = require('express-validator');
 var flash = require('connect-flash');
 var passport = require('passport');
-const MongoDBStore = require("connect-mongo")(session);
+
+
 
 
 var indexRoute= require('./routes/index')
 var dashRoute= require('./routes/dashboard')
 
 
-
-
 var app = express();
-
 
 mongoose.connect(process.env.MONGO_URL ||'mongodb://0.0.0.0:27017/growerDB',{
     useNewUrlParser:true,
@@ -34,7 +30,6 @@ mongoose.connect(process.env.MONGO_URL ||'mongodb://0.0.0.0:27017/growerDB',{
 mongoose.connection.on('connected',()=>{
     console.log('Mongoose is connected!!!')
 })
-
 
 
 
@@ -52,38 +47,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 
 app.use(cookieParser());
-
-const dbUrl =process.env.MONGO_URL ||'mongodb://0.0.0.0:27017/growerDB';
-mongoose.connect(process.env.MONGO_URL ||'mongodb://0.0.0.0:27017/growerDB',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-} )
-//app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
-
-const store = new MongoDBStore({
-    url: dbUrl,
-    secret,
-    touchAfter: 24 * 60 * 60
-});
-
-store.on("error", function (e) {
-    console.log("SESSION STORE ERROR", e)
-})
-const sessionConfig = {
-    store,
-    name: 'session',
-    secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        // secure: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
-app.use(session(sessionConfig));
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -93,13 +57,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
     res.locals.login = req.isAuthenticated();
     res.locals.session = req.session;
-    res.locals.currentUser = req.isAuthenticated();
-    res.locals.notUser = !req.isAuthenticated();
     res.locals.oldUrl = req.url;
     res.locals.message = req.session.message;
     req.session.message = null;
     next();
 });
+
+
 
 
 app.use('/', indexRoute);
@@ -108,8 +72,7 @@ app.use('/dash', dashRoute);
 
 
 
-
-const port = process.env.PORT || 6500;
+const port = process.env.PORT || 5500;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
